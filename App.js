c4dc617e47324f4be46c779d7d703ec42f733175
thesweetsqueeze <TheSweetSqueeze.ICT@gmail.com>
2025-06-30 00:05:31 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="The Sweet Squeeze Logo" style={{ width: '200px' }} />
+        <h1>Sip the Sunshine. Taste the Snow.</h1>
+        <p>Welcome to The Sweet Squeeze lemonade stand!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
